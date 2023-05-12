d1119cf9b58621ff98ff96c3eae837227e4ffa22
Yan0606 <yan060604@gmail.com>
2023-05-12 17:46:48 -0300

    second

---
+++ struct/equipeEsports.cs
@@ -0,0 +1,27 @@
+namespace Struct;
+
+public struct equipeEsporte{
+    
+    public string equipe;
+
+    public string registrarCampeonatoVencido;
+
+    public string atualizarValorTotalPremiacoes;
+
+    public string verificarAnoEstreia;
+
+    public float valor = 0;
+
+    // public void adicionarValor(float novoValor) {
+    //     this.valor = this.valor + novoValor;
+    // }
+
+    // public equipeEsporte(string equipe, string registrarCampeonatoVencido, string atualizarValorTotalPremiacoes, string verificarAnoEstreia){
+    //     this.equipe = equipe;
+    //     this.registrarCampeonatoVencido=registrarCampeonatoVencido;
+        
+    // }
+
+
+
+}
